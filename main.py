6d8d717f9ead33e6abc2666775a48918d8103aa5
Nir Ellor <nirellorwaizner@gmail.com>
2025-11-09 18:08:42 +0200

    added failed from BUICK and ready to download Buick

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dropbox.exceptions import ApiError
 
 ROOT_IMPORT_PATH = "/AdSpender/Vivvix Data/vivvix_reports_for_download"
 ROOT_EXPORT_PATH = "/AdSpender/Vivvix Data/Media_files"
-ONLY_ONE_FILE = "Buick_2024_Yearly_1711241.xlsx"
+ONLY_ONE_FILE = "Lincoln_2024_yearly_1711273.xlsx"
 SHEET_NAME = "Report"
 
 dbx = dropbox.Dropbox(
